c9de31802c6298fc3a1955b967679a2f97e46f9b
Virgil Bugnariu <bugnariu.virgil@gmail.com>
2019-11-07 19:31:19 +0200

    Renamed files

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
+const router = require('./router.js');
+const jsonParser = bodyParser.json();
 
-const router = require('./app/router.js');
+app.use(jsonParser);
 app.use(router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
